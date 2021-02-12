4316747ef78e7ee4065abd29f19694d29a00a12a
Itisafeature <thebigdebugger@gmail.com>
2021-02-12 17:31:24 -0500

    adding react hook form, basic board form

--- src/routes/BoardRoutes.js
+++ src/routes/BoardRoutes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import BoardsIndex from '../boards/BoardsIndex';
+import BoardsNew from '../boards/BoardsNew';
 
 const BoardRoutes = () => (
   <>
     <Route exact path="/boards">
       <BoardsIndex />
+    </Route>
+    <Route exact path="/boards/new">
+      <BoardsNew />
     </Route>
   </>
 );
