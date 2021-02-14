7c32092312a482332bb0ad8ce7109129da23f658
Itisafeature <thebigdebugger@gmail.com>
2021-02-14 12:46:42 -0500

    useFieldArray react-hook-form

--- src/boards/BoardsNew.jsx
+++ src/boards/BoardsNew.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import axios from 'axios';
-import { processForm } from '../helpers/helpers.js';
+import ItemFields from '../items/ItemFields';
 
 const BoardsNew = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const [itemCount, setItemCount] = useState(0);
+  const [items, setItems] = useState([]);
+  const { register, control, handleSubmit, errors } = useForm({
+    defaultValues: {
+      item: [{name: ""}]
+    }
+  });
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    control,
+    name: "item"
+  })
+
+  // const addItem = () => {
+    // setItemCount(itemCount + 1);
+    // setItems([...items, {id: itemCount}])
+  // }
 
   const onSubmit = async (data) => {
+    debugger;
     try {
       const res = await axios.post('/boards/new', data)
     } catch (err) {
@@ -18,6 +34,18 @@ const BoardsNew = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <input name="board[name]" type="text" ref={register({required: true})} />
       {errors.name && <span>This field is required</span>}
+      <ul>
+        {fields.map((item, index) => (
+            <input
+              name={`item${[index]}.name`}
+              ref={register({ required: true})}
+              defaultValue={item.name}
+              key={item.id}
+            />
+        ))
+        }
+      </ul>
+      <button type="button" onClick={() => append({name: ""})}>Add Item</button>
       <button type="submit">Create Board</button>
     </form>
   )
