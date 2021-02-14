95abce887204330769505162a8a7b7e97b3d0c10
Itisafeature <thebigdebugger@gmail.com>
2021-02-13 19:47:11 -0500

    working on creation of new board

--- src/boards/BoardsNew.jsx
+++ src/boards/BoardsNew.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { processForm } from '../helpers/helpers.js';
 
 const BoardsNew = () => {
   const { register, handleSubmit, errors } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data)
+  const onSubmit = async (data) => {
+    try {
+      const res = await axios.post('/boards/new', data)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="name" type="text" ref={register({required: true})} />
+      <input name="board[name]" type="text" ref={register({required: true})} />
       {errors.name && <span>This field is required</span>}
       <button type="submit">Create Board</button>
     </form>
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,3 +3,13 @@ export const titleize = str =>
     .split(' ')
     .map(word => word[0].toUpperCase() + word.slice(1))
     .join(' ');
+
+export const processForm = form => {
+  const formData = new FormData(form);
+  const formDataObj = {};
+  for (const key of formData.keys()) {
+    formDataObj[key] = formData.get(key);
+  }
+  console.log(formDataObj);
+  return formDataObj;
+};
