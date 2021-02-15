6d93963596b12b05c6c2bf7834839743b040eb2b
Itisafeature <thebigdebugger@gmail.com>
2021-02-14 20:06:07 -0500

    styling boards new form

--- src/boards/BoardsNew.jsx
+++ src/boards/BoardsNew.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message'
 import axios from 'axios';
-import ItemFields from '../items/ItemFields';
+import '../assets/BoardsNew.scss'
 
 const BoardsNew = () => {
   const [itemCount, setItemCount] = useState(0);
@@ -28,42 +28,50 @@ const BoardsNew = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="board[name]" type="text" ref={register({required: "Name of board is required"})} />
-      <ErrorMessage errors={errors} name="board[name]" render={({message}) => <p>{message}</p> }/>
-      <ul>
-        {fields.map((item, index) => (
-          <div key={item.id} className="item">
-            <h3>Item</h3>
-            <label>
-            item name  
-              <input
-                type="text"
-                name={`items[${index}].name`}
-                ref={register({ required: "Item name is required"})}
-                defaultValue={item.name}
-              />
-            </label>
-            <ErrorMessage errors={errors} name={`items[${index}].name`} render={({message}) => <p>{message}</p> }/>
-            <label>
-              <select defaultValue="" name={`items[${index}].status`} ref={register()}>
-              <option disabled value="">--- Select a Status ---</option>
-                {statuses.map((status) => <option key={status} value={status}>{status}</option>)}
-              </select>
-            </label>
-            <textarea
-              type="textarea"
-              name={`items[${index}].notes`}
-              ref={register()}
-              defaultValue={item.notes}
-            />
-          </div>
-        ))
-        }
-      </ul>
-      <button type="button" onClick={() => append({name: "", notes: ""})}>Add Item</button>
-      <button type="submit">Create Board</button>
-    </form>
+    <div className="board-form-container">
+      <form className="new-board-form" onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Name of Board
+          <input name="board[name]" type="text" ref={register({required: "Name of board is required"})} />
+        </label>
+        <ErrorMessage errors={errors} name="board[name]" render={({message}) => <p>{message}</p> }/>
+          {fields.map((item, index) => (
+            <div key={item.id} className="item">
+              <h3>Item</h3>
+              <label>
+              item name  
+                <input
+                  type="text"
+                  name={`items[${index}].name`}
+                  ref={register({ required: "Item name is required"})}
+                  defaultValue={item.name}
+                />
+              </label>
+              <ErrorMessage errors={errors} name={`items[${index}].name`} render={({message}) => <p>{message}</p> }/>
+              <label>
+                status
+                <select defaultValue="" name={`items[${index}].status`} ref={register()}>
+                <option disabled value="">--- Select a Status ---</option>
+                  {statuses.map((status) => <option key={status} value={status}>{status}</option>)}
+                </select>
+              </label>
+              <label>
+                notes
+                <textarea
+                  type="textarea"
+                  name={`items[${index}].notes`}
+                  ref={register()}
+                  defaultValue={item.notes}
+                />
+              </label>
+              <button type="button" onClick={() => remove(index)}>Remove Item</button>
+            </div>
+          ))
+          }
+        <button className="add-item" type="button" onClick={() => append({name: "", notes: ""})}>Add Item</button>
+        <button type="submit">Create Board</button>
+      </form>
+    </div>
   )
 }
 
