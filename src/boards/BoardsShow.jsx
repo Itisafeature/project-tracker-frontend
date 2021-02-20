4003ac2c1088f0684bab053aaf8b886ad483657d
Itisafeature <thebigdebugger@gmail.com>
2021-02-20 14:51:39 -0500

    good progress on new item modal

--- src/boards/BoardsShow.jsx
+++ src/boards/BoardsShow.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory, useParams } from 'react-router-dom';
 import ItemsContainer from '../items/ItemsContainer';
+import ItemsNew from '../items/ItemsNew';
 import '../assets/BoardsShow.scss';
 
 const BoardsShow = () => {
@@ -10,8 +11,7 @@ const BoardsShow = () => {
   const [loaded, setLoaded] = useState(false);
   const [board, setBoard] = useState(null);
   const [error, setError] = useState(false);
-
-
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     const boardState = history.location.state
@@ -23,24 +23,22 @@ const BoardsShow = () => {
         console.log(err.response)
       }
     }
-
-    if (boardState === undefined) {
-      getBoard();
-    } else {
-      setBoard(boardState)
-    }
-  
+    boardState === undefined ? getBoard() : setBoard(boardState)
   }, [boardName, history.location.state])
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  }
 
   if (board) {
     return (
       <div className="board-container">
         <div className="board-header">
-          <button className="add-item pseudo">Add Item</button>
           <h1 className="board_name">{board.name}</h1>
-          <button className="add-item">Add Item</button>
+          <button className="add-item" onClick={toggleForm}>Add Item</button>
         </div>
-        <ItemsContainer items={board.items} />
+        {showForm ? <ItemsNew /> : null }
+        <ItemsContainer items={board.items} /> 
       </div>
     )
   } else {
--- src/items/ItemsNew.jsx
+++ src/items/ItemsNew.jsx
@@ -1,46 +1,58 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message'
+import '../assets/ItemsNew.scss';
 
 
 const ItemsNew = () => {
   const { register, handleSubmit, errors } = useForm();
+  const statuses = ['Icebox', 'Not Started', 'In-Progress', 'Completed']
+
+  const onSubmit = () => {
+    debugger;
+  }
   return (
-    <form className="item-new_form" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        className="name"
-        name="name"
-        ref={register({required: "Name of item is required"})}
-      />
-      <ErrorMessage 
-        errors={errors} 
-        name="name" 
-        render={({message}) => <p className="item-new-error">{message}</p> }
-      />
-      <label htmlFor="status">Status</label>
-      <select 
-        className="item-status"
-        name="status"
-        ref={register({ required: "Status is required"})}
-      >
-      <option disabled value="">--- Select a Status ---</option>
-        {statuses.map((status) => <option key={status} value={status}>{status}</option>)}
-      </select>
-      <ErrorMessage 
-        errors={errors} 
-        name="statis"
-        render={({message}) => <p className="item-new-error">{message}</p> }
-      />
-      <label htmlFor="item-notes">Notes</label>
-      <textarea
-        type="textarea"
-        cols="30"
-        rows="6"
-        className="item-notes"
-        name="notes"
-        ref={register()}
-        defaultValue={item.notes}
-      />
-    </form>
+    <div className="item-new_container">
+      <h1>Add Item</h1>
+      <form className="item-new_form" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
+        <input
+          className="name"
+          name="name"
+          ref={register({required: "Name of item is required"})}
+        />
+        <ErrorMessage 
+          errors={errors} 
+          name="name" 
+          render={({message}) => <p className="item-new-error">{message}</p> }
+        />
+        <label htmlFor="status">Status</label>
+        <select 
+          className="item-status"
+          name="status"
+          ref={register({ required: "Status is required"})}
+        >
+        <option disabled value="">--- Select a Status ---</option>
+          {statuses.map((status) => <option key={status} value={status}>{status}</option>)}
+        </select>
+        <ErrorMessage 
+          errors={errors} 
+          name="statis"
+          render={({message}) => <p className="item-new-error">{message}</p> }
+        />
+        <label htmlFor="item-notes">Notes</label>
+        <textarea
+          type="textarea"
+          cols="30"
+          rows="6"
+          className="item-notes"
+          name="notes"
+          ref={register()}
+        />
+        <button className="add-item_button" type="Submit">Add Item</button>
+      </form>
+    </div>
   )
 }
+
+export default ItemsNew;
